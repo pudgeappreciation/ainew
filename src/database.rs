972bc2b0499cb702bf365da637fc78d1a4f6cb91
pudgeappreciation <187714606+pudgeappreciation@users.noreply.github.com>
2025-02-07 22:06:35 -0500

    refactor: move database code to src/database

---
+++ src/database.rs
@@ -0,0 +1,22 @@
+use sqlx::{Pool, Sqlite};
+
+pub async fn get_database() -> Pool<Sqlite> {
+    // Initiate a connection to the database file, creating the file if required.
+    let database = sqlx::sqlite::SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect_with(
+            sqlx::sqlite::SqliteConnectOptions::new()
+                .filename("sqlite.db")
+                .create_if_missing(true),
+        )
+        .await
+        .expect("Couldn't connect to database");
+
+    // Run migrations, which updates the database's schema to the latest version.
+    sqlx::migrate!("./migrations")
+        .run(&database)
+        .await
+        .expect("Couldn't run database migrations");
+
+    database
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,15 @@
+mod database;
 mod discord;
 mod draw_task;
 mod global;
 
+use database::get_database;
 use discord::bot::Bot;
 use global::channels::respond_to_message::{self};
 use global::channels::wake_draw_task::{self};
-use sqlx::{Pool, Sqlite};
-use tokio;
 
 use serenity::prelude::*;
-
-async fn get_database() -> Pool<Sqlite> {
-    // Initiate a connection to the database file, creating the file if required.
-    let database = sqlx::sqlite::SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect_with(
-            sqlx::sqlite::SqliteConnectOptions::new()
-                .filename("sqlite.db")
-                .create_if_missing(true),
-        )
-        .await
-        .expect("Couldn't connect to database");
-
-    // Run migrations, which updates the database's schema to the latest version.
-    sqlx::migrate!("./migrations")
-        .run(&database)
-        .await
-        .expect("Couldn't run database migrations");
-
-    database
-}
+use tokio;
 
 #[tokio::main]
 async fn main() {
