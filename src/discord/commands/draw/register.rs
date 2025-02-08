431ad2af3585175620ba38fc4a53d6e3eddc7662
pudgeappreciation <187714606+pudgeappreciation@users.noreply.github.com>
2025-02-08 13:03:03 -0500

    feature: add new options to draw command

--- src/discord/commands/draw/register.rs
+++ src/discord/commands/draw/register.rs
@@ -1,30 +1,24 @@
 use serenity::all::{Command, Context, GuildId};
-use serenity::builder::{CreateCommand, CreateCommandOption};
-use serenity::model::application::CommandOptionType;
+use serenity::builder::CreateCommand;
+
+use crate::discord::commands::option;
 
 fn create_command() -> CreateCommand {
     CreateCommand::new("draw")
         .description("draw an image")
+        .add_option(option::string("prompt", "The prompt to use").required(true))
+        .add_option(option::string("negative_prompt", "The negative_prompt to use").required(false))
         .add_option(
-            CreateCommandOption::new(CommandOptionType::String, "prompt", "The prompt to use")
-                .required(true),
+            option::string("size", "The size to use, formatted as {width}x{height}")
+                .required(false),
         )
-        .add_option(
-            CreateCommandOption::new(
-                CommandOptionType::String,
-                "negative_prompt",
-                "The negative_prompt to use",
-            )
-            .required(false),
-        )
-        .add_option(
-            CreateCommandOption::new(
-                CommandOptionType::Integer,
-                "steps",
-                "The steps to use, default 10",
-            )
-            .required(false),
-        )
+        .add_option(option::string("model", "The model to use").required(false))
+        .add_option(option::number("cfg_scale", "The cfg scale to use").required(false))
+        .add_option(option::int("steps", "The steps to use, default 10").required(false))
+        .add_option(option::string("sampler", "The sampler to use").required(false))
+        .add_option(option::string("scheduler", "The scheduler to use").required(false))
+        .add_option(option::int("clip_skip", "The clip skip to use").required(false))
+        .add_option(option::int("seed", "The seed to use").required(false))
 }
 
 pub async fn register(ctx: &Context) {
---
+++ src/discord/commands/option.rs
@@ -0,0 +1,13 @@
+use serenity::all::{CommandOptionType, CreateCommandOption};
+
+pub fn int(name: impl Into<String>, description: impl Into<String>) -> CreateCommandOption {
+    CreateCommandOption::new(CommandOptionType::Integer, name, description)
+}
+
+pub fn string(name: impl Into<String>, description: impl Into<String>) -> CreateCommandOption {
+    CreateCommandOption::new(CommandOptionType::String, name, description)
+}
+
+pub fn number(name: impl Into<String>, description: impl Into<String>) -> CreateCommandOption {
+    CreateCommandOption::new(CommandOptionType::Number, name, description)
+}
