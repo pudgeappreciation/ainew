39962705231437d6123491491bb0050b969d6729
pudgeappreciation <187714606+pudgeappreciation@users.noreply.github.com>
2025-02-14 00:02:30 -0500

    fix: fix some model names being quoted

--- src/global/models/civitai_info.rs
+++ src/global/models/civitai_info.rs
@@ -21,7 +21,13 @@ impl CivitaiInfo {
 
         let version = info.get("name")?.as_str()?.to_string();
         let base_model = info.get("baseModel")?.as_str()?.to_string();
-        let name = info.get("model")?.as_object()?.get("name")?.to_string();
+        let name = info
+            .get("model")?
+            .as_object()?
+            .get("name")?
+            .to_string()
+            .trim_matches('"')
+            .to_string();
         let version_id = info.get("id")?.as_i64()?;
         let model_id = info.get("modelId")?.as_i64()?;
 
