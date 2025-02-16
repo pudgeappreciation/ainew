2469799776e07f47926f084daa2dabdd0d5cc30b
pudgeappreciation <187714606+pudgeappreciation@users.noreply.github.com>
2025-02-15 22:06:11 -0500

    style: remove unnecessary option required definitions

--- src/discord/commands/draw/create.rs
+++ src/discord/commands/draw/create.rs
@@ -6,35 +6,28 @@ pub fn create() -> CreateCommand {
     CreateCommand::new("draw")
         .description("draw an image")
         .add_option(option::string("prompt", "The prompt to use").required(true))
-        .add_option(option::string("negative_prompt", "The negative_prompt to use").required(false))
+        .add_option(option::string(
+            "negative_prompt",
+            "The negative_prompt to use",
+        ))
         .add_option(
             option::string(
                 "size",
                 "The size to use, formatted as {width}x{height}, in pixels",
             )
-            .required(false)
             .set_autocomplete(true),
         )
-        .add_option(
-            option::string("model", "The model to use")
-                .required(false)
-                .set_autocomplete(true),
-        )
-        .add_option(option::number("cfg_scale", "The cfg scale to use").required(false))
-        .add_option(option::int("steps", "The steps to use, default 10").required(false))
-        .add_option(
-            option::string("sampler", "The sampler to use")
-                .required(false)
-                .set_autocomplete(true),
-        )
+        .add_option(option::string("model", "The model to use").set_autocomplete(true))
+        .add_option(option::number("cfg_scale", "The cfg scale to use"))
+        .add_option(option::int("steps", "The steps to use, default 10"))
+        .add_option(option::string("sampler", "The sampler to use").set_autocomplete(true))
         .add_option(
             option::string(
                 "scheduler",
                 "The scheduler to use (Leaving this blank is usually best)",
             )
-            .required(false)
             .set_autocomplete(true),
         )
-        .add_option(option::int("clip_skip", "The clip skip to use").required(false))
-        .add_option(option::int("seed", "The seed to use").required(false))
+        .add_option(option::int("clip_skip", "The clip skip to use"))
+        .add_option(option::int("seed", "The seed to use"))
 }
--- src/discord/commands/loras/create.rs
+++ src/discord/commands/loras/create.rs
@@ -5,9 +5,5 @@ use crate::discord::commands::option;
 pub fn create() -> CreateCommand {
     CreateCommand::new("loras")
         .description("display the availible loras")
-        .add_option(
-            option::int("page", "Jump to a specific page")
-                .required(false)
-                .min_int_value(1),
-        )
+        .add_option(option::int("page", "Jump to a specific page").min_int_value(1))
 }
