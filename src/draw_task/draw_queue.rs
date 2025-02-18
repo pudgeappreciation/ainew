8302acdc0126663fabe7a1d3de4bb678c7aefb85
pudgeappreciation <187714606+pudgeappreciation@users.noreply.github.com>
2025-02-18 00:06:11 -0500

    feature: fix user queue query

--- src/draw_task/draw_queue.rs
+++ src/draw_task/draw_queue.rs
@@ -29,36 +29,54 @@ impl From<DbDrawRequest> for DrawRequest {
 pub async fn get_next_user(database: &Pool<Sqlite>) -> Option<i64> {
     sqlx::query_scalar!(
         r#"
-        SELECT `user_id` as `user_id!`
+        SELECT `user_id`
         FROM (
             SELECT
                 `user_id`,
                 'default' AS `state`,
                 0 AS `created_at`
             FROM `draw_requests`
+            WHERE
+                NOT EXISTS (
+                    SELECT 1
+                    FROM `draw_requests` AS `draw_requests_inner`
+                    WHERE 
+                        `state` NOT IN ('queued')
+                        AND `draw_requests_inner`.`user_id` = `draw_requests`.`user_id`
+                )
             UNION ALL
             SELECT
                 `user_id`,
                 `state`,
                 `created_at`
             FROM `draw_requests`
+            WHERE
+                EXISTS (
+                    SELECT 1
+                    FROM `draw_requests` AS `draw_requests_inner`
+                    WHERE 
+                        `state` IN ('queued')
+                        AND `draw_requests_inner`.`user_id` = `draw_requests`.`user_id`
+                )
         )
         WHERE `state` NOT IN ('queued')
         GROUP BY `user_id`
-        ORDER BY MAX(`created_at`) DESC
+        ORDER BY MAX(`created_at`) ASC
         "#,
     )
     .fetch_optional(database)
     .await
     .unwrap_or(None)
+    .flatten()
 }
 
 pub async fn get_next_request(database: &Pool<Sqlite>) -> Option<DrawRequest> {
     let Some(user_id) = get_next_user(database).await else {
+        println!("No next user");
         return None;
     };
 
-    sqlx::query_as!(
+    let request = sqlx::query_as!(
         DbDrawRequest,
         r#"
         SELECT
@@ -77,7 +95,14 @@ pub async fn get_next_request(database: &Pool<Sqlite>) -> Option<DrawRequest> {
         user_id,
     )
     .fetch_optional(database)
-    .await
-    .unwrap_or(None)
-    .map(|request| request.into())
+    .await;
+
+    match request {
+        Ok(request) => request.map(|r| r.into()),
+        Err(why) => {
+            println!("Error fetching request: {:?}", why);
+
+            None
+        }
+    }
 }
